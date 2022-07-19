3d774bd2e5b6c4c726559fadc86d4d453bfb531b
HugoSando <hugosa291@gmail.com>
2022-07-18 21:57:13 -0600

    let y const

--- src/es6/index.js
+++ src/es6/index.js
@@ -22,3 +22,31 @@ let epicPhrase = hello + ' ' + world;
 console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+
+
+// multilinea
+
+let lorem = 'Qui consequatur. Commmodi. Ipsum vel duis yet minima \n'
++ 'otra frase epica que necesitamos.';
+// es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+// Desestructuracion de elementos
+
+let person = {
+    'name' : 'oscar',
+    'age': 32,
+    'country': 'MX'
+}
+
+console.log(person.name, person.age, person.country);
+
+
+let { name, age, country } = person;
+console.log(name, age, country);
