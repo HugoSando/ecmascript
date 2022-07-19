498a00cc5dcafe8d98fa305623415d6fde1f3c0f
HugoSando <hugosa291@gmail.com>
2022-07-18 22:35:07 -0600

    Import, export y Generators

--- src/es6/index.js
+++ src/es6/index.js
@@ -131,6 +131,44 @@ helloPromise()
     .catch(error => console.log(error));
 
 
+// CLASES
+
+class calculator {
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
 
 
+//IMPORT Y EXPORT
 
+import { hello } from ',/module';
+
+hello();
+
+
+//GENERATORS
+
+function* helloWorld() {
+    if (true) {
+        yield 'hello, ';
+    }
+    if (true) {
+        yield 'world';
+    }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
